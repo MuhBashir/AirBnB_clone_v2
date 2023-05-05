3008f5cb7164a80858ff885b434704f5e99ca4bc
root <mbashiribrahim7@gmail.com>
2023-05-05 04:25:47 +0000

    deploy webstatic

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -2,7 +2,7 @@
 import os
 from fabric.api import *
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['107.22.141.177', '100.25.171.131']
 
 
 def do_clean(number=0):
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,7 +6,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['100.25.171.131,'107.22.141.177']
 
 
 def do_pack():
